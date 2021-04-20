349dac9abb33f3f8ddba79aa83044a5dd245b547
Rafael Dutra <dutrarafaeloliveira@gmail.com>
2021-04-20 19:30:12 -0300

    Criação do projeto: Cria projeto.

---
+++ branchCRUD-funcionario/ciss/src/main/java/com/ciss/CissApplication.java
@@ -0,0 +1,13 @@
+package com.ciss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CissApplication.class, args);
+	}
+
+}
---
+++ branchCRUD-funcionario/ciss/src/test/java/com/ciss/CissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
